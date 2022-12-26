3dec5d8362c8e08d0719d9972055e234583c2f66
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-12-26 11:29:01 +0000

    2022年12月26日 - 06

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,10 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function() {
-
+  test('It should correctly read a whole number input', function() {
+    assert.equal(convertHandler.getNum('3.1mi'), 3.1);
+  });
+  test('It should correctly read a decimal number input', function() {
+    assert.equal(convertHandler.getNum('310mi'), 310);
+  });
 });
