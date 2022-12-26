ebebdcc3f5a1bfc9899fe6834fb538b79f4dcf09
MakotoAraki <85997616+Makoto-Araki@users.noreply.github.com>
2022-12-26 08:58:21 +0000

    2022年12月26日 - 01

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,26 +1,49 @@
 function ConvertHandler() {
   
+  // Regular expressions
+  const reg1 = /[0-9.]+/;
+  const reg2 = /km|mi|L|gal|lbs|kg/;
+
+  // Get number value from input(req.query.input)
   this.getNum = function(input) {
-    let result;
-    
+    let result = input.match(reg1);
     return result;
   };
-  
+
+  // Get unit name from input(req.query.input)
   this.getUnit = function(input) {
-    let result;
-    
+    let result = input.match(reg2);
     return result;
   };
-  
+
+  // Get the coresponding unit name from input(req.query.input)
   this.getReturnUnit = function(initUnit) {
     let result;
-    
+    switch(initUnit) {
+      case 'km':
+        result = 'mi';
+        break;
+      case 'mi':
+        result = 'km';
+        break;
+      case 'L':
+        result = 'gal';
+        break;
+      case 'gal':
+        result = 'L';
+        break;
+      case 'lbs':
+        result = 'kg';
+        break;
+      case 'kg':
+        result = 'lbs';
+        break;
+    }
     return result;
   };
 
   this.spellOutUnit = function(unit) {
     let result;
-    
     return result;
   };
   
--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,14 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
+
+  // Instance of my handmade functions
   let convertHandler = new ConvertHandler();
 
+  // GET - URL/api/convert
+  app.route('/api/convert').get((req, res) => {
+    console.log(`数値 : ${convertHandler.getNum(req.query.input)}`);
+    console.log(`単位 : ${convertHandler.getUnit(req.query.input)}`);
+  });
+  
 };
